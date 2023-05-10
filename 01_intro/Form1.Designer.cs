933add130d39f115f351673b30bab4a8f8a38fcd
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-05-10 16:15:21 +0300

    Add project files.

---
+++ 01_intro/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace _01_intro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.coordsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mainLabel.Location = new System.Drawing.Point(309, 148);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(521, 65);
+            this.mainLabel.TabIndex = 0;
+            this.mainLabel.Text = "Hello Windows Forms";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(461, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Push Me";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.MyBtn_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(535, 545);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 80);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.ExitBtnClick);
+            // 
+            // coordsLabel
+            // 
+            this.coordsLabel.AutoSize = true;
+            this.coordsLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.coordsLabel.Location = new System.Drawing.Point(405, 46);
+            this.coordsLabel.Name = "coordsLabel";
+            this.coordsLabel.Size = new System.Drawing.Size(333, 45);
+            this.coordsLabel.TabIndex = 3;
+            this.coordsLabel.Text = "Cursor Coords: 45 : 42";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1148, 667);
+            this.Controls.Add(this.coordsLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mainLabel);
+            this.Name = "Form1";
+            this.Text = "Hello Windows Forms!";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label mainLabel;
+        private Button button1;
+        private Button button2;
+        private Label coordsLabel;
+    }
+}
---
+++ 01_intro/Form1.cs
@@ -0,0 +1,58 @@
+namespace _01_intro
+{
+    public partial class Form1 : Form
+    {
+        // properties...
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // create new button
+            Button myBtn = new Button();
+            myBtn.Location = new System.Drawing.Point(761, 492);
+            myBtn.Size = new System.Drawing.Size(300, 100);
+            myBtn.Text = "Dynamic Created";
+            myBtn.UseVisualStyleBackColor = false;
+
+            myBtn.Click += MyBtn_Click;
+
+            this.Controls.Add(myBtn);
+        }
+
+        // event handlers...
+
+        private void MyBtn_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Clicked!");
+        }
+
+        private void ExitBtnClick(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to close the Window?", 
+                                         "Exit?", MessageBoxButtons.YesNo, 
+                                         MessageBoxIcon.Question, 
+                                         MessageBoxDefaultButton.Button2);
+
+            if (result == DialogResult.Yes)
+                this.Close();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            // change label properties
+            mainLabel.Text += "!";
+            mainLabel.ForeColor = Color.Brown;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            mainLabel.ForeColor = Color.Black;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.coordsLabel.Text = $"Cursor position: {e.X} : {e.Y}";
+        }
+    }
+}
