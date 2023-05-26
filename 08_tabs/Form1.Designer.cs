74566057b4f6c32f5f2c4875934e560a2e43e05a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-05-26 15:47:02 +0300

    Tab Control example.

--- 08_tabs/Form1.Designer.cs
+++ 08_tabs/Form1.Designer.cs
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 78);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1455, 770);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(8, 46);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1439, 716);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Page 1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(585, 441);
+            button1.Name = "button1";
+            button1.Size = new Size(284, 70);
+            button1.TabIndex = 2;
+            button1.Text = "Show Message";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += ShowMessageBtnClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(448, 245);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(554, 143);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(598, 116);
+            label1.Name = "label1";
+            label1.Size = new Size(271, 78);
+            label1.TabIndex = 0;
+            label1.Text = "Tab Body";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(8, 46);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1439, 716);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Page 2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(32, 32);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1479, 42);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(141, 36);
+            toolStripButton1.Text = "New Tab";
+            toolStripButton1.Click += NewTabBtnClick;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(151, 36);
+            toolStripButton2.Text = "Close Tab";
+            toolStripButton2.Click += toolStripButton2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1479, 860);
+            Controls.Add(toolStrip1);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private TabPage tabPage2;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
     }
 }
--- 08_tabs/Form1.cs
+++ 08_tabs/Form1.cs
@@ -2,9 +2,78 @@ namespace _08_tabs
 {
     public partial class Form1 : Form
     {
+        public TextBox SelectedTextBox
+        {
+            get
+            {
+                return tabControl1.SelectedTab.Controls.OfType<TextBox>().First();
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void NewTabBtnClick(object sender, EventArgs e)
+        {
+            string name = $"Page {tabControl1.TabPages.Count + 1}";
+
+            var tabTextBox = new TextBox()
+            {
+                Location = new Point(448, 245),
+                Multiline = true,
+                Size = new Size(554, 143),
+                TabIndex = 1
+            };
+
+            var tabBtn = new Button()
+            {
+                Location = new Point(585, 441),
+                Size = new Size(284, 70),
+                TabIndex = 2,
+                Text = "Show Message",
+                UseVisualStyleBackColor = true
+            };
+            tabBtn.Click += (sender, args) =>
+            {
+                MessageBox.Show(tabTextBox.Text);
+            };
+            //tabBtn.Click += ShowMessageBtnClick;
+
+            var tabLabel = new Label
+            {
+                AutoSize = true,
+                Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point),
+                Location = new Point(598, 116),
+                Size = new Size(271, 78),
+                TabIndex = 0,
+                Text = $"{name} Body!"
+            };
+
+            var newTab = new TabPage(name)
+            {
+                Location = new Point(8, 46),
+                Padding = new Padding(3),
+                Size = new Size(1439, 716),
+                TabIndex = 0,
+                UseVisualStyleBackColor = true
+            };
+            newTab.Controls.Add(tabBtn);
+            newTab.Controls.Add(tabTextBox);
+            newTab.Controls.Add(tabLabel);
+
+            tabControl1.TabPages.Add(newTab);
+        }
+
+        private void ShowMessageBtnClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(SelectedTextBox.Text);
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex != -1)
+                tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
         }
     }
 }
